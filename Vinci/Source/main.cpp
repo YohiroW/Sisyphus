40c462d1b505425505ab59c0df5636de9056d3a6
YohiroW <yohiro36@foxmail.com>
2021-07-07 00:26:12 +0800

    Added vulkan debug message output

--- Vinci/Source/main.cpp
+++ Vinci/Source/main.cpp
@@ -11,7 +11,6 @@ const uint32_t HEIGHT = 600;
 const char* APPNAME = "VINCI";
 
 #ifdef _DEBUG
-// Used validation layer from LunarG
 const std::vector<const char*> VALIDATION_LAYERS = { "VK_LAYER_RENDERDOC_Capture" };
 
 bool checkValidationLayerSupport()
@@ -41,7 +40,43 @@ bool checkValidationLayerSupport()
 	}
 	return true;
 }
+
+VkResult createDebugUtilsMessengerEXT(VkInstance instance,
+	                                  const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+	                                  const VkAllocationCallbacks* pAllocator,
+	                                  VkDebugUtilsMessengerEXT* pCallback)
+{
+	auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+	if (func != nullptr) 
+	{
+		return func(instance, pCreateInfo, pAllocator, pCallback);
+	}
+	else 
+	{
+		return VK_ERROR_EXTENSION_NOT_PRESENT;
+	}
+}
+
+void destroyDebugUtilsMessengerEXT(VkInstance instance, 
+                                   VkDebugUtilsMessengerEXT debugMessenger,
+                                   const VkAllocationCallbacks* pAllocator) 
+{
+	auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+	if (func != nullptr) 
+	{
+		func(instance, debugMessenger, pAllocator);
+	}
+}
 #endif
+
+std::vector<const char*> getRequiredExts(const uint32_t& glfwExtCount, const char** glfwExtensions)
+{
+	std::vector<const char*> extensions(glfwExtensions, glfwExtensions+ glfwExtCount);
+
+	extensions.push_back("VK_EXT_DEBUG_UTILS_EXTENSION_NAME");
+
+	return extensions;
+}
 
 class HelloTriangleApplication {
 public:
@@ -55,11 +90,67 @@ public:
 protected:
 	void createVulkanInstance(const uint32_t& glfwExtCount, const char** glfwExtensions);
 
+#ifdef _DEBUG
+	// SEVERITY
+	// used for diagnostic 
+	// VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT£º  0x0001
+
+	// used for resources creation
+	// VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT£º     0x0010
+
+	// warning and error
+	// VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT£º  0x0100
+	// VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT£º    0x1000
+
+	// MESSAGE TYPE
+	// VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT       0x01
+	// VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT    0x02
+	// VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT   0x04
+
+	static VKAPI_ATTR VkBool32 debugVKCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, 
+										       VkDebugUtilsMessageTypeFlagsEXT messageType, 
+		                                       const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, 
+		                                       void* pUserData)
+	{
+		std::cerr << "[DEBUG]: " << pCallbackData->pMessage << std::endl;
+		
+		return VK_FALSE;
+	}
+
+	void SetupDebugInfo()
+	{
+		VkDebugUtilsMessengerCreateInfoEXT debugInfo = {};
+		debugInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+		
+		debugInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | 
+			                        VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT | 
+			                        VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | 
+			                        VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+		
+		debugInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+			                    VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+			                    VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+
+		debugInfo.pfnUserCallback = debugVKCallback;
+		debugInfo.pUserData = nullptr;
+
+		if (createDebugUtilsMessengerEXT(vulkanInstance, &debugInfo, nullptr, &debugMessenger) != VK_SUCCESS) 
+		{
+			throw std::runtime_error("failed to set up debug messenger!");
+		}
+	}
+#endif
+
 private:
 	GLFWwindow* window;
 	VkInstance vulkanInstance;
 
-	void initWindow() {
+#ifdef _DEBUG
+	VkDebugUtilsMessengerEXT debugMessenger;
+#endif
+
+	void initWindow() 
+	{
 		glfwInit();
 
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
@@ -68,21 +159,31 @@ private:
 		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
 	}
 
-	void initVulkan() {
+	void initVulkan() 
+	{
 		uint32_t glfwExtCount = 0;
 		const char** glfwExtensions;
+
 		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtCount);
 
 		createVulkanInstance(glfwExtCount, glfwExtensions);
+
+		SetupDebugInfo();
 	}
 
-	void mainLoop() {
-		while (!glfwWindowShouldClose(window)) {
+	void mainLoop() 
+	{
+		while (!glfwWindowShouldClose(window)) 
+		{
 			glfwPollEvents();
 		}
 	}
 
-	void cleanup() {
+	void cleanup() 
+	{
+#ifdef _DEBUG
+		destroyDebugUtilsMessengerEXT(vulkanInstance, debugMessenger, nullptr);
+#endif
 		vkDestroyInstance(vulkanInstance, nullptr);
 
 		glfwDestroyWindow(window);
@@ -125,13 +226,37 @@ void HelloTriangleApplication::createVulkanInstance(const uint32_t& glfwExtCount
 	VkInstanceCreateInfo createInfo{};
 	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 	createInfo.pApplicationInfo = &appInfo;
-	createInfo.enabledExtensionCount = glfwExtCount;
-	createInfo.ppEnabledExtensionNames = glfwExtensions;
 #ifdef _DEBUG
 	createInfo.enabledLayerCount = static_cast<uint32_t>(VALIDATION_LAYERS.size());
 	createInfo.ppEnabledLayerNames = VALIDATION_LAYERS.data();
+
+	auto extensions = getRequiredExts(glfwExtCount, glfwExtensions);
+	createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+	createInfo.ppEnabledExtensionNames = extensions.data();
+
+	VkDebugUtilsMessengerCreateInfoEXT debugInfo = {};
+	{
+		
+		debugInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+
+		debugInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+			VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT |
+			VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+			VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+
+		debugInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+			VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+			VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+
+		debugInfo.pfnUserCallback = debugVKCallback;
+		debugInfo.pUserData = nullptr;
+	}
+	createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*)&debugInfo;
 #else
 	createInfo.enabledLayerCount = 0;
+
+	createInfo.enabledExtensionCount = glfwExtCount;
+	createInfo.ppEnabledExtensionNames = glfwExtensions;
 #endif
 
 	if (vkCreateInstance(&createInfo, nullptr, &vulkanInstance) != VK_SUCCESS) {
