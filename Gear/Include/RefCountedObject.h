75cc525371a958815224cd2a632767307439225a
Yohiro <yohiro36@foxmail.com>
2021-08-18 15:57:15 +0800

    Update RefCountedObject.h

--- Gear/Include/RefCountedObject.h
+++ Gear/Include/RefCountedObject.h
@@ -1,1 +1,18 @@
+#pragma once
+#define BEGIN_NAMESPACE_GEAR namespace Gear {
+#define END_NAMESPACE  }
 
+BEGIN_NAMESPACE_GEAR 
+
+template<typename T>
+class RefCountedPtr
+{
+public:
+    RefCountedPtr();
+    RefCountedPtr(T* ptr);
+    RefCountedPtr(const RefCountedPtr& other);
+    ~RefCountedPtr();
+    
+}
+
+END_NAMESPACE
