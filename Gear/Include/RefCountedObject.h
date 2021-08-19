d772596a3dfd9d128123bd37fb8ca04a11460803
Yohiro <yohiro36@foxmail.com>
2021-08-19 14:55:11 +0800

    Implement interfaces for RefCountedObject and RefCountedPtr

--- Gear/Include/RefCountedObject.h
+++ Gear/Include/RefCountedObject.h
@@ -75,4 +75,144 @@ private:
     T* Ptr;
 }
 
+//////////////////////////////////////////////////////////////////////
+// Inline implementation
+
+inline void RefCountedObject::Increase()
+{
+    Atomic::Increment(ReferenceCount);
+}
+
+inline void RefCountedObject::Decrease()
+{
+    int32 refCount = Atomic::Decrement(ReferenceCount);
+    
+    if(refCount< 1)
+    {
+       Delete(); 
+    }
+}
+
+inline int32 GetRefCount()
+{
+    return referenceCount;
+}
+
+inline bool IsShared()
+{
+    return referenceCount > 1;
+}
+
+
+template<typename T>
+inline RefCountedPtr<T>::RefCountedPtr(T* ptr): 
+ Ptr(ptr)
+ {
+     // avoid LHS
+     if(ptr)
+     {
+         ptr->Increase();
+     }
+ }
+
+template<typename T>
+inline RefCountedPtr<T>::~RefCountedPtr()
+{
+    T* ptr = Ptr;
+    if(ptr)
+    {
+        ptr->Decrease();
+    }
+}
+
+template<typename T>
+inline RefCountedPtr<T>& RefCountedPtr<T>::operator = (const RefCountedPtr& other)
+{
+    // Not thread safe
+    T* newPtr = other.Ptr;
+    if(newPtr)
+    {
+        newPtr->Increase();
+    }
+    
+    T* oldPtr = Ptr;
+    Ptr = newPtr;
+    
+    // Safe operation
+    if(oldPtr)
+    {
+        oldPtr->Decrease();
+    }
+    
+    return (*this);
+}
+
+template<typename T>
+inline RefCountedPtr<T>& RefCountedPtr<T>::operator = (T* other)
+{
+    if(other)
+    {
+        other->Increase();
+    }
+    
+    T* oldPtr = Ptr;
+    Ptr = other;
+    
+    // Safe operation
+    if(oldPtr)
+    {
+        oldPtr->Decrease();
+    }
+    
+    return (*this);
+}
+
+template<typename T>
+inline bool RefCountedPtr<T>::operator == (const T* other) const
+{
+    return Ptr == other;
+}
+
+template<typename T>
+inline bool RefCountedPtr<T>::operator == (const RefCountedPtr& other) const
+{
+    return Ptr == other.Ptr;
+}
+
+template<typename T>
+inline bool RefCountedPtr<T>::operator != (const T* other) const
+{
+    return Ptr != other;
+}
+
+template<typename T>
+inline bool RefCountedPtr<T>::operator != (const RefCountedPtr& other) const
+{
+    return Ptr != other.Ptr;
+}
+
+template<typename T>
+inline T* RefCountedPtr<T>::operator -> () const
+{
+    return Ptr;
+}
+
+template<typename T>
+inline T* RefCountedPtr<T>::GetPtr() const
+{
+    return Ptr;
+}
+
+template<typename T>
+inline void RefCountedPtr<T>::Release()
+{
+    T* ptr = Ptr;
+    Ptr = nullptr;
+    
+    if(ptr)
+    {
+        ptr->Decrease();
+    }
+}
+
 END_NAMESPACE
