282d1d78eda22ce5ee16cb9fdf6995c65d4a53ce
Yohiro <yohiro36@foxmail.com>
2021-09-03 10:26:12 +0800

    Fixed space appears in sdk path caused shaders failed to compile

--- Vinci/Shader/CompileShader.bat
+++ Vinci/Shader/CompileShader.bat
@@ -2,5 +2,5 @@ echo off
 
 cd %dp0
 
-%VULKAN_SDK%/Bin/glslangValidator.exe -V DummyVertexShader.vert
-%VULKAN_SDK%/Bin/glslangValidator.exe -V DummyPixelShader.frag
+"%VULKAN_SDK%/Bin/glslangValidator.exe" -V DummyVertexShader.vert
+"%VULKAN_SDK%/Bin/glslangValidator.exe" -V DummyPixelShader.frag
